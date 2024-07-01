0f229594db8392d7cb4332b08046929bb571237c
Nick Solly <solstice035+github@gmail.com>
2024-07-01 18:44:47 +0100

    chore: Update form and template names for data loading (from upload)

--- forms.py
+++ forms.py
@@ -4,9 +4,9 @@ from wtforms.validators import DataRequired
 
 
 class LoadForm(FlaskForm):
-    file = FileField("Upload Excel File", validators=[DataRequired()])
+    file = FileField("Load Excel File", validators=[DataRequired()])
     start_row = IntegerField("Start Row", default=1, validators=[DataRequired()])
     service_line = StringField(
         "Service Line", default="Consulting", validators=[DataRequired()]
     )
-    submit = SubmitField("Upload")
+    submit = SubmitField("Load")
